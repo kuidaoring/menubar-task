aca43508ed2b30c670d45f929bb11b14eebbeef4
kuidaoring <kuijob@gmail.com>
2023-12-03 00:21:37 +0900

    リストの見た目の調整

--- renderer/pages/task.tsx
+++ renderer/pages/task.tsx
@@ -1,5 +1,6 @@
 import {
   Checkbox,
+  IconButton,
   List,
   ListItem,
   ListItemButton,
@@ -7,17 +8,34 @@ import {
   ListItemText,
 } from "@mui/material";
 
+import AddIcon from "@mui/icons-material/Add";
+import MinuxIcon from "@mui/icons-material/Remove";
+
 const TaskPage = () => {
   return (
     <List sx={{ py: 0 }}>
       {[0, 1, 2, 3, 4, 5, 6, 7].map((value) => {
         return (
-          <ListItem key={value} disablePadding>
-            <ListItemButton sx={{ py: 0 }}>
+          <ListItem
+            key={value}
+            disablePadding
+            divider
+            dense
+            secondaryAction={
+              <IconButton edge="end">
+                {value % 2 == 0 ? <AddIcon /> : <MinuxIcon />}
+              </IconButton>
+            }
+          >
+            <ListItemButton disableGutters>
               <ListItemIcon>
-                <Checkbox />
+                <Checkbox size="small" />
               </ListItemIcon>
-              <ListItemText primary={`task${value}`} secondary={`2023/12/02`} />
+              <ListItemText
+                primary={`task${value} 日本語だとこんな感じになるので、長い文章だとどうなんるですかね`}
+                primaryTypographyProps={{ sx: { typography: "body2" } }}
+                secondary={`2023/12/02`}
+              />
             </ListItemButton>
           </ListItem>
         );
